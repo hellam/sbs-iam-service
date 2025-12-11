3e914e5a3ac7e121351c12d8bd5bb2e6b07c6972
hellamimbosa <hellamixon@gmail.com>
2025-12-11 12:56:56 +0300

    update security configuration to allow access to Swagger UI and API docs

--- src/main/java/ke/shiva/sbs_iam/config/SecurityConfig/SecurityConfig.java
+++ src/main/java/ke/shiva/sbs_iam/config/SecurityConfig/SecurityConfig.java
@@ -31,8 +31,8 @@ public class SecurityConfig {
                                 "/oauth/mfa/**",
                                 "/oauth/finalize/**",
                                 "/oauth/password/**",
-                                "/v3/api-docs",
-                                "/swagger-ui.html"
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 );
