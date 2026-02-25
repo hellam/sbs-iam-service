2c44434f0760caa413a5d1f516228485bccbba16
Hellix <hellamixon@gmail.com>
2026-02-25 14:40:22 +0300

    feat(account): Implement IamUserService for retrieving primary contact information

---
+++ src/main/java/ke/shiva/sbs_iam/modules/iam/app/service/IamUserService.java
@@ -0,0 +1,51 @@
+package ke.shiva.sbs_iam.modules.iam.app.service;
+
+import ke.shiva.sbs_iam.modules.iam.domain.entity.identity.IamUserEntity;
+import ke.shiva.sbs_iam.modules.iam.domain.entity.identity.UserContact;
+import ke.shiva.sbs_iam.modules.iam.domain.enums.ContactType;
+import ke.shiva.sbs_iam.modules.iam.infra.repository.IamUserRepository;
+import ke.shiva.sbs_iam.modules.iam.infra.repository.UserContactRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class IamUserService {
+
+    private final IamUserRepository iamUserRepository;
+    private final UserContactRepository userContactRepository;
+
+    /**
+     * Get Iam User Primary Email and Phone Number by User ID.
+     */
+
+    public Map<String, String> getUserPrimaryContactInfo(IamUserEntity iamUser) {
+
+        List<UserContact> userContact =userContactRepository.findByIamUserAndPrimaryIsTrue(iamUser)
+                .orElseThrow(() -> new IllegalStateException("No primary contacts found for user: " + iamUser.getId()));
+        String email = null;
+        String phoneNumber = null;
+        //loop and set by contact type
+        for(UserContact contact : userContact){
+            if(contact.getContactType().equals(ContactType.EMAIL)){
+                email = contact.getContactValue();
+            }
+            if(contact.getContactType().equals(ContactType.PHONE)){
+                phoneNumber = contact.getContactValue();
+            }
+        }
+
+        assert email != null;
+        assert phoneNumber != null;
+        return Map.of(
+                "email", email,
+                "phone", phoneNumber
+        );
+    }
+
+}
